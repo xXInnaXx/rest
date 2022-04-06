ec0b6df6321da0ac1ec57fdb072012e16abe77e3
innakhabirova <iikhabi@mail.ru>
2022-04-06 14:33:45 +0300

    check failed test

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -6,25 +6,25 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
 
 class MobileBankApiTestV3 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200)
-          // специализированные проверки - лучше
-          .contentType(ContentType.JSON)
-          .body("", hasSize(3))
-          .body("[0].currency", equalTo("RUB"))
-          .body("[0].balance", greaterThanOrEqualTo(0))
-      ;
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // специализированные проверки - лучше
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[0].currency", oneOf("RUM", "USD"))
+                .body("[0].balance", greaterThanOrEqualTo(0));
     }
 }
--- src/test/java/ru/netology/rest/MobileBankApiTestV5.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV5.java
@@ -20,7 +20,7 @@ class MobileBankApiTestV5 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(200)
+          .statusCode(201)
           .contentType(ContentType.JSON)
           .body("every{ it.balance >= 0 }", is(true))
       ;
